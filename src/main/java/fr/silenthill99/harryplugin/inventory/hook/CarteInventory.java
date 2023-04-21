551d833fa897b4c91ba9f9c786b82b3222866628
silenthill99 <florian.graziani@sfr.fr>
2023-04-21 17:59:23 +0200

    correction de détails liés à la carte du maraudeur

--- src/main/java/fr/silenthill99/harryplugin/inventory/hook/CarteInventory.java
+++ src/main/java/fr/silenthill99/harryplugin/inventory/hook/CarteInventory.java
@@ -54,7 +54,7 @@ public class CarteInventory extends AbstractInventory<CarteHolder> {
     public enum CarteDuMaraudeur
     {
         BUREAU("Bureau de Dumbledore", new Location(world, -1985.5, 195, 430.5, 180.0f, 0)),
-        QUIDDITCH("Terrain de Quidditch", new Location(world, -1731.5, 83, 722.5)),
+        QUIDDITCH("Terrain de Quidditch", new Location(world, -1731.5, 83, 722.5, 0, 0)),
         GRANDE_SALLE("Grande salle", new Location(world, -2007.5, 121, 393.5, -135f, 0)),
         GRYFFONDOR("Salle commune de Gryffondor", new Location(world, -1940.5, 133, 478.5, -45f, 0)),
         TOILETTES("Toilettes de Mimi Geignarde", new Location(world, -1978.5, 123, 479.5, 90f, 0)),
@@ -62,14 +62,14 @@ public class CarteInventory extends AbstractInventory<CarteHolder> {
         PONT("Pont couvert", new Location(world, -1864.5, 124, 454.5, -90f, 0)),
         SERPENTARD("Salle commune de Serpentard", new Location(world, -2114.5, 6, 486.5, 90f, 0)),
         CABANE("Cabane d'Hagrid", new Location(world, -1717.5, 73, 373.5, -135, 0)),
-        BIBLIOTHEQUE("Bibliothèque", new Location(world, -1963.5, 134, 534.5)),
-        TOMBE("Tombe de Dumbledore", new Location(world, -2079.5, 55, 267.5)),
-        ESCALIERS("Grands escaliers", new Location(world, -1994.5, 130, 416.5)),
-        SAULE_COGNEUR("Saule cogneur", new Location(world, -1944.5, 121, 661.5)),
-        TOUR_DASTRONOMIE("Tour d'astronomie", new Location(world, -1998.5, 205, 545.5)),
-        COUR_DE_METAMORPHOSE("Cour de métamorphose", new Location(world, -1987.5, 121, 582.5)),
-        SALLE_DE_METAMORPHOSE("Salle de métamorphoses", new Location(world, -2025.5, 122, 583.5, 90f, 0f)),
-        CHAMBRE("Chambre des secrets", new Location(world, -1901.5, 39, 520.5));
+        BIBLIOTHEQUE("Bibliothèque", new Location(world, -1963.5, 134, 534.5, 90f, 0)),
+        TOMBE("Tombe de Dumbledore", new Location(world, -2079.5, 55, 267.5, 45f, 0)),
+        ESCALIERS("Grands escaliers", new Location(world, -1994.5, 130, 416.5, 0, 0)),
+        SAULE_COGNEUR("Saule cogneur", new Location(world, -1944.5, 121, 661.5, -180f, 0)),
+        TOUR_DASTRONOMIE("Tour d'astronomie", new Location(world, -1998.5, 205, 545.5, 45, 0)),
+        COUR_DE_METAMORPHOSE("Cour de métamorphose", new Location(world, -1987.5, 121, 582.5, 180, 0)),
+        SALLE_DE_METAMORPHOSE("Salle de métamorphoses", new Location(world, -2025.5, 122, 582.5, 90f, 0f)),
+        CHAMBRE("Chambre des secrets", new Location(world, -1901.5, 38, 520.5, -90, 0));
 
         private final String name;
         private final Location loc;
